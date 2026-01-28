5b11574e88803980143d6548dda9edc34c6468d8
Dennis Miranda <dennise.miranda.15@gmail.com>
2026-01-27 21:01:07 -0500

    Add editTask functionality

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import TaskForm from "@/components/TaskForm";
 import TaskList from "@/components/TaskList";
+import { Task } from "@/models/task";
 import { useState } from "react";
 
 export default function Home() {
-  const [tasks, setTasks] = useState<task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const saveTask = (description: string) => {
     setTasks([
@@ -17,6 +18,14 @@ export default function Home() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const editTask = (id: string, newDescription: string) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, description: newDescription } : task
+      )
+    );
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -24,7 +33,7 @@ export default function Home() {
         <TaskForm saveTask={saveTask} />
         <div className="w-full">
           <h2 className="text-lg font-semibold py-2">Tasks List</h2>
-          <TaskList tasks={tasks} deleteTask={deleteTask} />
+          <TaskList tasks={tasks} deleteTask={deleteTask} editTask={editTask} />
         </div>
       </main>
     </div>
--- frontend/components/TaskList.tsx
+++ frontend/components/TaskList.tsx
@@ -1,9 +1,34 @@
+"use client";
+import { Task } from "@/models/task";
+import { useState } from "react";
+
 interface TaskListProps {
-  tasks: task[];
+  tasks: Task[];
   deleteTask: (id: string) => void;
+  editTask: (id: string, newDescription: string) => void;
 }
 
-export default function TaskList({ tasks, deleteTask }: TaskListProps) {
+export default function TaskList({
+  tasks,
+  deleteTask,
+  editTask,
+}: TaskListProps) {
+  const [editingTaskId, setEditTaskId] = useState<string | null>(null);
+  const [editValue, setEditValue] = useState<string>("");
+
+  const handleEditClick = (task: Task) => {
+    setEditTaskId(task.id);
+    setEditValue(task.description);
+  };
+
+  const handleSaveEdit = (id: string) => {
+    if (!editValue.trim()) return;
+
+    editTask(id, editValue);
+    setEditTaskId(null);
+    setEditValue("");
+  };
+
   return (
     <ul className="w-full">
       {tasks.map((task) => (
@@ -11,9 +36,36 @@ export default function TaskList({ tasks, deleteTask }: TaskListProps) {
           <div className="flex gap-2">
             <input type="checkbox" />
             <p>{task.description}</p>
+
+            {/* Input para editar */}
+            {editingTaskId === task.id ? (
+              <input
+                type="text"
+                value={editValue}
+                className="border border-gray-200 p-1 rounded"
+                onChange={(e) => setEditValue(e.target.value)}
+              />
+            ) : (
+              <p>{task.description}</p>
+            )}
           </div>
-          <div className="flex gap-2">
-            <button className="bg-blue-300 px-2 py-1 rounded">Editar</button>
+          <div className="flex gap-2 ml-4">
+            {editingTaskId === task.id ? (
+              <button
+                className="bg-green-300 px-2 py-1 rounded"
+                onClick={() => handleSaveEdit(task.id)}
+              >
+                Save edit
+              </button>
+            ) : (
+              <button
+                className="bg-blue-300 px-2 py-1 rounded"
+                onClick={() => handleEditClick(task)}
+              >
+                Editar
+              </button>
+            )}
+
             <button
               className="bg-red-400 px-2 py-1 rounded"
               onClick={() => deleteTask(task.id)}
