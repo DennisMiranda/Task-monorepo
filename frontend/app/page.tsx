e7ba40d5b6317f200c6725e9ca7060d0ae47a232
Dennis Miranda <dennise.miranda.15@gmail.com>
2026-01-27 20:03:14 -0500

    Enhance UI page and add Task form component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import TaskForm from "@/components/TaskForm";
 import TaskList from "@/components/TaskList";
 import { useState } from "react";
 
@@ -9,11 +10,22 @@ export default function Home() {
     { id: "3", description: "Task 3", completed: false },
   ]);
 
+  const saveTask = (description: string) => {
+    setTasks([
+      ...tasks,
+      { id: (tasks.length + 1).toString(), description, completed: false },
+    ]);
+  };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>Tasks</h1>
-        <TaskList tasks={tasks} />
+        <h1 className="text-2xl font-bold py-2">Tasks</h1>
+        <TaskForm saveTask={saveTask} />
+        <div className="w-full">
+          <h2 className="text-lg font-semibold py-2">Tasks List</h2>
+          <TaskList tasks={tasks} />
+        </div>
       </main>
     </div>
   );
